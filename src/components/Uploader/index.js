f7c7e3fd0f43cc83dfe036fd68fe2a7893f83b41
alixez <alixe.z@foxmail.com>
2019-01-05 20:33:35 +0800

    fix uploader limit

--- src/components/Uploader/index.js
+++ src/components/Uploader/index.js
@@ -11,14 +11,16 @@ function getBase64(img, callback) {
 }
 
 function beforeUpload(file) {
-  const isJPG = file.type === 'image/jpeg';
-  if (!isJPG) {
-    message.error('You can only upload JPG file!');
-  }
+  // const isJPG = file.type === 'image/jpeg';
+  // if (!isJPG) {
+  //   message.error('You can only upload JPG file!');
+  // }
+  const isJPG = true
   const isLt2M = file.size / 1024 / 1024 < 2;
   if (!isLt2M) {
-    message.error('Image must smaller than 2MB!');
+    message.error('图片需要小于 2MB!');
   }
+  
   return isJPG && isLt2M;
 }
 
